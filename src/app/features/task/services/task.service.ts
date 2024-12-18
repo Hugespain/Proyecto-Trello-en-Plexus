930a50adbe4c251e50abfa65812c0d34c0dfffd4
Hugespain <diegosilva3795@gmail.com>
2024-12-18 12:11:10 +0100

    Angular Material instalado y errores de versiones ESlint solucionado

--- src/app/features/task/services/task.service.ts
+++ src/app/features/task/services/task.service.ts
@@ -5,5 +5,5 @@ import { Injectable } from '@angular/core';
 })
 export class TaskService {
 
-  constructor() { }
+  constructor() {'' }
 }
--- src/app/shared/services/validation.service.ts
+++ src/app/shared/services/validation.service.ts
@@ -5,5 +5,5 @@ import { Injectable } from '@angular/core';
 })
 export class ValidationService {
 
-  constructor() { }
+  constructor() {'' }
 }
